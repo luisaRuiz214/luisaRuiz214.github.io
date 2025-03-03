23837fdce183c59c0f518e9e27896992f2ad6993
luisaRuiz214 <luferum@gmail.com>
2025-03-03 16:30:43 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+// Función para manejar el envío del formulario
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevenir el comportamiento predeterminado de envío del formulario
+
+    // Obtener los valores del formulario
+    const usuario = document.getElementById("usuario").value;
+    const contraseña = document.getElementById("contraseña").value;
+
+    // Validar el usuario y la contraseña
+    if (usuario === "luisa" && contraseña === "12345") {
+        // Si el usuario y la contraseña son correctos, redirigir al index.html
+        window.location.href = "/Index.html"; // Redirige al archivo de actividades
+    } else {
+        // Si los datos son incorrectos, mostrar un mensaje de error
+        alert("Usuario o contraseña incorrectos");
+    }
+});
+
